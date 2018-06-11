ba34afb04643de77a66bf405d6b063598541cfcd
SweeneyTheBarber <37616255+SweeneyTheBarber@users.noreply.github.com>
2018-06-11 12:21:16 +0430

    env PORT

--- modules/config.js
+++ modules/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    port : 8080,
+    port : process.env.PORT || 8080,
     secret : '&=L^XHy!MPw2e**#KAk3R@SMFT3nK+QGN2YkZkcaPPptEky4*PjhD@AaNz++*2Jr',
     path : {
         app : {
